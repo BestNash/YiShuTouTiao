b5ef39f71eb4cf4fa65d169d7662927d455ced56
yishu <yishu@yishudeMac-mini.local>
2015-05-16 11:50:45 +0800

    Initial Commit

---
+++ YiShuTouTiao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YiShuTouTiao
+//
+//  Created by yishu on 15/5/16.
+//  Copyright (c) 2015年 yishu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
